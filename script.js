ff523fff9162794f2d45d784f7fd07573fad5042
Hikaru2000 <30906913+Hikaru2000@users.noreply.github.com>
2023-07-23 16:20:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const apiKey = '7a47f7c1221e45aa9a85e81c98bbb23f';
 const apiUrl = 'https://api.themoviedb.org/3';
+const resultsPerPage = 10; // Number of results per page
+
+let currentPage = 1;
+let currentSearchTerm = '';
+let currentMethod = 'search';
 
 const searchInput = document.getElementById('searchInput');
 const searchBtn = document.getElementById('searchBtn');
@@ -10,46 +15,54 @@ const resultsContainer = document.getElementById('results');
 searchBtn.addEventListener('click', () => {
   const searchTerm = searchInput.value.trim();
   if (searchTerm) {
-    searchMoviesAndShows(searchTerm);
+    currentMethod = 'search';
+    currentSearchTerm = searchTerm;
+    currentPage = 1;
+    searchMoviesAndShows(currentSearchTerm, currentPage);
   }
 });
 
 searchMethodBtn.addEventListener('click', () => {
   searchInput.placeholder = 'Enter keyword';
+  currentMethod = 'search';
+  currentPage = 1;
+  searchMoviesAndShows(currentSearchTerm, currentPage);
 });
 
 discoverMethodBtn.addEventListener('click', () => {
   searchInput.placeholder = 'Discovering...';
-  discoverMoviesAndShows();
+  currentMethod = 'discover';
+  currentPage = 1;
+  discoverMoviesAndShows(currentPage);
 });
 
-function searchMoviesAndShows(keyword) {
-  const searchUrl = `${apiUrl}/search/multi?api_key=${apiKey}&query=${encodeURIComponent(keyword)}`;
-  
+function searchMoviesAndShows(keyword, page) {
+  const searchUrl = `${apiUrl}/search/multi?api_key=${apiKey}&query=${encodeURIComponent(keyword)}&page=${page}`;
+
   fetch(searchUrl)
     .then(response => response.json())
     .then(data => {
-      showResults(data.results);
+      showResults(data.results, data.total_pages);
     })
     .catch(error => {
       console.error('Error fetching data:', error);
     });
 }
 
-function discoverMoviesAndShows() {
-  const discoverUrl = `${apiUrl}/discover/movie?api_key=${apiKey}&sort_by=popularity.desc`;
+function discoverMoviesAndShows(page) {
+  const discoverUrl = `${apiUrl}/discover/movie?api_key=${apiKey}&sort_by=popularity.desc&page=${page}`;
 
   fetch(discoverUrl)
     .then(response => response.json())
     .then(data => {
-      showResults(data.results);
+      showResults(data.results, data.total_pages);
     })
     .catch(error => {
       console.error('Error fetching data:', error);
     });
 }
 
-function showResults(results) {
+function showResults(results, totalPages) {
   resultsContainer.innerHTML = '';
 
   results.forEach(result => {
@@ -67,5 +80,34 @@ function showResults(results) {
 
     resultsContainer.appendChild(item);
   });
+
+  // Pagination
+  const pagination = document.createElement('div');
+  pagination.classList.add('pagination');
+
+  if (currentPage > 1) {
+    const prevBtn = createPaginationButton('Prev', currentPage - 1);
+    pagination.appendChild(prevBtn);
+  }
+
+  if (currentPage < totalPages) {
+    const nextBtn = createPaginationButton('Next', currentPage + 1);
+    pagination.appendChild(nextBtn);
+  }
+
+  resultsContainer.appendChild(pagination);
 }
 
+function createPaginationButton(text, page) {
+  const button = document.createElement('button');
+  button.textContent = text;
+  button.addEventListener('click', () => {
+    if (currentMethod === 'search') {
+      searchMoviesAndShows(currentSearchTerm, page);
+    } else {
+      discoverMoviesAndShows(page);
+    }
+    currentPage = page;
+  });
+  return button;
+}
