94b13dee6e72842ebf6ba52ff907aaaa9088c561
Hikaru2000 <30906913+Hikaru2000@users.noreply.github.com>
2023-07-23 14:38:03 -0400

    Update App.js

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./components/Home";
+import Search from "./components/Search";
+import MovieDetail from "./components/MovieDetail";
+import PersonDetail from "./components/PersonDetail";
+import Favorites from "./components/Favorites";
+import Watchlist from "./components/Watchlist";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/search" component={Search} />
+        <Route exact path="/movie/:id" component={MovieDetail} />
+        <Route exact path="/person/:id" component={PersonDetail} />
+        <Route exact path="/favorites" component={Favorites} />
+        <Route exact path="/watchlist" component={Watchlist} />
+      </Switch>
+    </Router>
   );
 }
 
