d84fa178e9b1b2795914dcaf08b9179c608988f0
Hikaru2000 <30906913+Hikaru2000@users.noreply.github.com>
2023-07-23 16:32:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+//API Key to access Movie API
 const apiKey = '7a47f7c1221e45aa9a85e81c98bbb23f';
 const apiUrl = 'https://api.themoviedb.org/3';
+
+//Results per page for pagination
 const resultsPerPage = 10; // Number of results per page
 
+// Pagination elements
 let currentPage = 1;
 let currentSearchTerm = '';
 let currentMethod = 'search';
@@ -12,6 +16,7 @@ const searchMethodBtn = document.getElementById('searchMethod');
 const discoverMethodBtn = document.getElementById('discoverMethod');
 const resultsContainer = document.getElementById('results');
 
+//Search button Event - Starts and returns search
 searchBtn.addEventListener('click', () => {
   const searchTerm = searchInput.value.trim();
   if (searchTerm) {
@@ -22,6 +27,7 @@ searchBtn.addEventListener('click', () => {
   }
 });
 
+//Search Method Button Event - Goes back to search actions
 searchMethodBtn.addEventListener('click', () => {
   searchInput.placeholder = 'Enter keyword';
   currentMethod = 'search';
@@ -29,6 +35,7 @@ searchMethodBtn.addEventListener('click', () => {
   searchMoviesAndShows(currentSearchTerm, currentPage);
 });
 
+//Discovery Method Button Event - Goes to discovery mode of popular movies
 discoverMethodBtn.addEventListener('click', () => {
   searchInput.placeholder = 'Discovering...';
   currentMethod = 'discover';
@@ -36,6 +43,7 @@ discoverMethodBtn.addEventListener('click', () => {
   discoverMoviesAndShows(currentPage);
 });
 
+//Search Function
 function searchMoviesAndShows(keyword, page) {
   const searchUrl = `${apiUrl}/search/multi?api_key=${apiKey}&query=${encodeURIComponent(keyword)}&page=${page}`;
 
@@ -49,6 +57,7 @@ function searchMoviesAndShows(keyword, page) {
     });
 }
 
+//Discovery Function - shows popular movies and shows
 function discoverMoviesAndShows(page) {
   const discoverUrl = `${apiUrl}/discover/movie?api_key=${apiKey}&sort_by=popularity.desc&page=${page}`;
 
@@ -62,6 +71,7 @@ function discoverMoviesAndShows(page) {
     });
 }
 
+//Displays results and constructs card
 function showResults(results, totalPages) {
   resultsContainer.innerHTML = '';
 
@@ -110,4 +120,73 @@ function createPaginationButton(text, page) {
     currentPage = page;
   });
   return button;
+}
+
+
+// Add event listener to show detailed info when clicking on a movie/TV show
+resultsContainer.addEventListener('click', (event) => {
+  const movieItem = event.target.closest('.movie-item');
+  if (movieItem) {
+    const id = movieItem.dataset.id;
+    showDetailedInfo(id);
+  }
+});
+
+function showDetailedInfo(id) {
+  const detailsUrl = `${apiUrl}/movie/${id}?api_key=${apiKey}&append_to_response=credits,reviews`;
+
+  fetch(detailsUrl)
+    .then(response => response.json())
+    .then(data => {
+      showDetailedModal(data);
+    })
+    .catch(error => {
+      console.error('Error fetching movie details:', error);
+    });
+}
+
+function showDetailedModal(movieData) {
+  // Create a modal to display detailed information
+  const modal = document.createElement('div');
+  modal.classList.add('modal');
+  modal.innerHTML = `
+    <div class="modal-content">
+      <span class="close-btn">&times;</span>
+      <h2>${movieData.title}</h2>
+      <p>Release Date: ${movieData.release_date}</p>
+      <p>Overview: ${movieData.overview}</p>
+      
+      <h3>Cast</h3>
+      <ul class="cast-list">
+        ${getCastList(movieData.credits.cast)}
+      </ul>
+
+      <h3>Reviews</h3>
+      <ul class="reviews-list">
+        ${getReviewsList(movieData.reviews.results)}
+      </ul>
+    </div>
+  `;
+
+  document.body.appendChild(modal);
+
+  // Close the modal when clicking on the close button
+  const closeBtn = modal.querySelector('.close-btn');
+  closeBtn.addEventListener('click', () => {
+    document.body.removeChild(modal);
+  });
+}
+
+function getCastList(cast) {
+  return cast
+    .slice(0, 5) // Display the first 5 cast members
+    .map(member => `<li>${member.name} as ${member.character}</li>`)
+    .join('');
+}
+
+function getReviewsList(reviews) {
+  return reviews
+    .slice(0, 5) // Display the first 5 reviews
+    .map(review => `<li>${review.author}: ${review.content}</li>`)
+    .join('');
 }
